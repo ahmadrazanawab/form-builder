c0241433f9614aa8e7e26ae405bb3073979217b5
ahmadrazanawab <ahmadrazanawab@gmail.com>
2024-12-15 19:53:13 +0530

    delete extra file

--- form-builder-backend/src/Routes/formRoutes.js
+++
@@ -1,8 +0,0 @@
-const express = require("express");
-const router = express.Router();
-const formController = require("../controller/formController");
-
-router.post("/forms", formController.createForm);
-router.get("/forms/:id", formController.getForm);
-
-module.exports = router;
--- form-builder-backend/src/formModel.js
+++
@@ -1,9 +0,0 @@
-const db = require('./db/db');
-
-exports.createForm = (data, callback) => {
-  db.query('INSERT INTO forms SET ?', data, callback);
-};
-
-exports.getFormById = (id, callback) => {
-  db.query('SELECT * FROM forms WHERE id = ?', [id], callback);
-};
